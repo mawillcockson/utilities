be3f0f89c684b161e4124bb8dc7bc926ae471e71
Matthew Willcockson <matthew@willcockson.family>
2021-01-03 18:17:53 -0600

    ci(github-actions): add GitHub Actions workflow and supporting scripts

---
+++ checks.py
@@ -0,0 +1,129 @@
+"""
+Runs the checks for each repository
+"""
+import sys
+from pathlib import Path
+from subprocess import run, CompletedProcess
+from typing import List, NamedTuple, Optional
+
+assert sys.version_info >= (3, 7), "Need Python 3.7+"
+
+
+class CIFiles(NamedTuple):
+    "all of the build files"
+    checks: List[Path]
+
+
+def in_git_dir(path: Path) -> bool:
+    """
+    check if a file or folder is under .git/
+
+    This is a very cheap check, and won't catch symlinks or hardlinks, etc
+    """
+    return ".git" in path.parts
+
+
+def find_ci_files(path: Path = Path(".")) -> CIFiles:
+    "list all the python files used by the ci system"
+    # List the check files
+    check_files = [path for path in Path(".").glob("**/checks.py") if not in_git_dir(path)]
+    return CIFiles(checks=check_files)
+
+
+def run_tool(args: List[str], cwd: Optional[Path] = None, check: bool = True) -> CompletedProcess:
+    "passes standard arguments to subprocess.run"
+    print(f"exec> {' '.join(args)}")
+    result = run(args=args, cwd=cwd, capture_output=True, text=True, check=check)
+    print(result.stdout)
+    if result.stderr:
+        print(result.stderr)
+    return result
+
+
+def run_py(path: Path) -> None:
+    "runs a .py file with python"
+    assert path.is_file(), "path must be a file"
+    assert path.suffix == ".py", "file must be a .py file"
+    run_tool([sys.executable, str(path)], cwd=path.parent, check=False)
+
+
+def check(project_dir: Path) -> None:
+    "runs the check.py in a project directory"
+    assert project_dir.is_dir(), "Project directory must be a directory"
+    path = project_dir.resolve(strict=True)
+    check_file = path / "checks.py"
+    assert check_file.is_file(), "check file must be a file"
+    run_py(path=check_file)
+
+
+def sub_projects(path: Path = Path(".")) -> List[Path]:
+    "list the project directories"
+    project_directories = list()
+    for item in path.glob("*"):
+        if not in_git_dir(item) and item.is_dir():
+            project_directories.append(item)
+
+    return project_directories
+
+
+def run_pip(args: List[str]) -> None:
+    "runs pip with the specified arguments"
+    run_tool([sys.executable, "-m", "pip"] + args)
+
+
+def pipx_install(args: List[str]) -> None:
+    "run pipx install with the specified arguments"
+    run_tool([sys.executable, "-m", "pipx", "install"] + args)
+
+
+def check_ci_files(ci_files: CIFiles) -> None:
+    "lints and type checks all of the CI files"
+    # Upgrade pip
+    run_pip("install --user --upgrade pip setuptools wheel".split())
+
+    # Install pipx
+    run_pip("install --user --upgrade pipx".split())
+
+    # Install linting tools
+    pipx_install(["isort"])
+    pipx_install(["black"])
+    pipx_install(["pylint"])
+    pipx_install(["mypy"])
+
+    # Find linting tool executables
+    pipx_bin = Path("~/.local/bin").expanduser().resolve(strict=True)
+    assert pipx_bin.is_dir(), "pipx_bin must be a directory"
+    isort_exe = pipx_bin / "isort"
+    black_exe = pipx_bin / "black"
+    pylint_exe = pipx_bin / "pylint"
+    mypy_exe = pipx_bin / "mypy"
+
+    # Find mypy.ini
+    mypy_ini = Path("./mypy.ini").resolve(strict=True)
+    assert mypy_ini.is_file(), "mypy.ini must be a file"
+
+    # Run tools
+    for path in ci_files.checks:
+        if not path.suffix == ".py":
+            raise NotImplementedError("can only check .py files")
+
+        assert path.is_file(), "paths must be files"
+        assert path.suffix == ".py", "paths must be python files"
+        run_tool(
+            [str(isort_exe), "--profile=black", "--python-version=auto", str(path)]
+        )
+        run_tool([str(black_exe), str(path)])
+        run_tool([str(pylint_exe), str(path)], cwd=path.parent)
+        run_tool([str(mypy_exe), "--config-file", str(mypy_ini), str(path)])
+
+
+def main() -> None:
+    "main function that's run when this file is called as a script"
+    for project_dir in sub_projects():
+        check(project_dir=project_dir)
+
+    check_ci_files(ci_files=find_ci_files())
+
+
+if __name__ == "__main__":
+    main()
---
+++ copy_unique/checks.py
@@ -0,0 +1,374 @@
+# pylint: disable=unsubscriptable-object, inherit-non-class
+# pylint: disable=too-many-arguments, too-few-public-methods
+"""
+runs all the checks
+
+this assumes that this file is located in the project directory
+
+The general workflow is:
+
+- Make sure pyenv is installed
+- Find the currently supported versions of Python 3.7+ from pyenv install --list
+- Install those versions, and the other needed versions
+- pyenv local supported-cpython
+- Make sure poetry is installed
+- Run `poetry install --verbose --no-interaction` to install the project and
+  dependencies using the system python version
+- Run tox for tests
+"""
+import json
+import os
+import re
+import subprocess
+import sys
+from pathlib import Path
+from shutil import which
+from subprocess import CompletedProcess
+from typing import Dict, List, Mapping, NamedTuple, Optional, Tuple, cast
+from warnings import warn
+
+PYENV_INSTALL_SCRIPT_URL = (
+    "https://github.com/pyenv/pyenv-installer/raw/master/bin/pyenv-installer"
+)
+PYENV_WIN_RELEASES_URL = "https://api.github.com/repos/pyenv-win/pyenv-win/releases"
+PYENV_WIN_REPO_URL = "https://github.com/pyenv-win/pyenv-win.git"
+cpython_version_re = re.compile(
+    r"^\s*(?P<major>2|3)\.(?P<minor>\d)\.(?P<micro>\d+)\s*$"
+)
+
+
+ReleasesJson = List[Dict[str, str]]
+
+
+class CPythonVersion(NamedTuple):
+    "CPython interpreter version specifier"
+    major: int
+    minor: int
+    micro: int
+
+
+def run(
+    args: List[str],
+    input_to_stdin: Optional[str] = None,
+    cwd: Optional[Path] = None,
+    check: bool = True,
+    print_stdout: bool = True,
+    env: Optional[Mapping[str, str]] = None,
+) -> CompletedProcess[str]:
+    "run subprocess.run with common arguments"
+    print(f"exec> {' '.join(args)}")
+
+    result = subprocess.run(
+        args=args,
+        input=input_to_stdin,
+        cwd=cwd,
+        check=check,
+        env=env,
+        capture_output=True,
+        text=True,
+    )
+
+    if print_stdout:
+        print(result.stdout)
+    if result.stderr:
+        print(result.stderr)
+    return result
+
+
+def check_present(command_name: str, strict: bool = False) -> Optional[Path]:
+    "check if a command is available"
+    print(f"checking for '{command_name}'")
+    path = which(command_name)
+    if path is None and strict:
+        raise FileNotFoundError(f"No command '{command_name}' found")
+
+    if path is None:
+        return None
+
+    return Path(path).resolve(strict=True)
+
+
+def pyenv_check_and_update() -> None:
+    "run pyenv-doctor to make sure all the build requirements exist"
+    run(["pyenv", "doctor"])
+    run(["pyenv", "update"])
+
+
+def add_to_github_path(path: Path) -> None:
+    "if running on a GitHub Actions runner, persist path across steps"
+    # NOTE:CI the GITHUB_PATH environment variable points to a file that is
+    # read bottom to top to build the PATH environment variable for subsequent
+    # actions
+    # From:
+    # https://docs.github.com/en/free-pro-team@latest/actions/reference/workflow-commands-for-github-actions#adding-a-system-path
+    github_path = os.getenv("GITHUB_PATH", default=None)
+    if not github_path:
+        # The "GITHUB_PATH" environment variable only exists in GitHub Actions
+        # runners
+        return
+
+    print(f"adding '{path}' for GITUB_PATH")
+    with open(github_path, mode="a") as file:
+        file.write(str(path.resolve(strict=True)))
+
+
+def add_to_path(path: Path) -> None:
+    "prepend a path to PATH"
+    absolute_path = path.resolve(strict=True)
+    current_path = os.environ["PATH"]
+    if str(absolute_path) not in current_path.split(sep=os.pathsep):
+        print(f"adding '{absolute_path}' to $PATH")
+        os.environ["PATH"] = os.pathsep.join((str(absolute_path), current_path))
+    add_to_github_path(absolute_path)
+    bashrc = Path("~/.bashrc").expanduser().resolve()
+    if not bashrc.is_file():
+        return
+
+    bashrc_text = bashrc.read_text()
+    if str(absolute_path) in bashrc_text:
+        return
+
+    with bashrc.open(mode="a") as file:
+        print(f"adding '{absolute_path}' to ~/.bashrc")
+        bash_path = os.pathsep.join((str(absolute_path), "${PATH}"))
+        file.write(f'export PATH="{bash_path}"')
+
+
+def add_pyenv_to_path(pyenv_root: Path) -> None:
+    "add pyenv executables and shims to the PATH"
+    pyenv_shims = pyenv_root / "shims"
+    pyenv_bin = pyenv_root / "bin"
+    # Add in reverse order so shims come first, just like how pyenv-installer
+    # and "pyenv init -" set it up
+    add_to_path(pyenv_bin)
+    add_to_path(pyenv_shims)
+
+
+def install_pyenv_linux() -> None:
+    """
+    These install steps are followed from:
+    https://github.com/pyenv/pyenv-installer#install
+    """
+    # Check if pyenv is already installed
+    pyenv_path = check_present("pyenv")
+    if pyenv_path:
+        warn("pyenv already installed")
+        pyenv_check_and_update()
+        return
+
+    pyenv_root = Path().home().resolve(strict=True) / ".pyenv"
+
+    if pyenv_root.exists() and not pyenv_root.is_dir():
+        raise FileExistsError(f"'{pyenv_root}' already exists, but is not a directory")
+
+    if pyenv_root.is_dir():
+        warn("pyenv not added to path")
+    else:
+        print("installing pyenv...")
+        # Make sure git, bash, and curl are installed
+        check_present("git", strict=True)
+        check_present("bash", strict=True)
+        check_present("curl", strict=True)
+
+        # pylint: disable=line-too-long
+
+        # Download installer script
+        script = run(
+            ["curl", "-L", "--output", "-", PYENV_INSTALL_SCRIPT_URL],
+            print_stdout=False,
+        ).stdout
+        if not script:
+            sys.exit("No pyenv script returned????")
+
+        # Run bash with the install script
+        run(["bash"], input_to_stdin=script)
+
+    add_pyenv_to_path(pyenv_root=pyenv_root)
+    pyenv_check_and_update()
+
+
+def install_pyenv_windows() -> None:
+    """
+    These install steps are taken from:
+    https://github.com/pyenv-win/pyenv-win#installation
+    """
+    powershell_exe = str(check_present("powershell", strict=True))
+    pyenv_win_releases_raw = run(
+        ["curl", "-L", "--output", "-", PYENV_WIN_RELEASES_URL], print_stdout=False
+    ).stdout
+    json_error_msg = "Problem with decoding GittHub release data for pyenv-win"
+    try:
+        pyenv_win_releases: ReleasesJson = cast(
+            ReleasesJson, json.loads(pyenv_win_releases_raw)
+        )
+    except json.decoder.JSONDecodeError as err:
+        raise ValueError(json_error_msg) from err
+
+    if not pyenv_win_releases:
+        raise ValueError(json_error_msg)
+
+    latest_release_data = pyenv_win_releases[0]
+    latest_release_tag = latest_release_data.get("tag_name", default=None)
+    if not latest_release_tag:
+        raise ValueError(json_error_msg)
+
+    run(
+        [
+            powershell_exe,
+            "-C",
+            (
+                "git clone "
+                "--depth 1"
+                f"--branch {latest_release_tag}"
+                f"{PYENV_WIN_REPO_URL}"
+                '"$HOME/.pyenv"'
+            ),
+        ]
+    )
+
+    pyenv_win_root = r'"\.pyenv\pyenv-win\"'
+    user = '"User"'
+    run(
+        [
+            powershell_exe,
+            (
+                "[System.Environment]::SetEnvironmentVariable("
+                "'PYENV',"
+                f"$env:USERPROFILE + {pyenv_win_root},{user}"
+                ")"
+            ),
+        ]
+    )
+
+    pyenv_win_bin = r'"\.pyenv\pyenv-win\bin;"'
+    pyenv_win_shims = r'"\.pyenv\pyenv-win\shims;"'
+    run(
+        [
+            powershell_exe,
+            "-C",
+            (
+                "[System.Environment]::SetEnvironmentVariable("
+                "'path',"
+                f"$HOME + {pyenv_win_bin} + "
+                f"$HOME + {pyenv_win_shims} + $env:Path,"
+                f"{user}"
+                ")"
+            ),
+        ]
+    )
+
+    pyenv_check_and_update()
+
+
+def pyenv_stable_cpython() -> List[str]:
+    """
+    list the latest stable CPython versions in each branch, formatted for pyenv
+
+    from:
+    https://devguide.python.org/#status-of-python-branches
+    """
+    stable_branch_tuples = [(3, 6), (3, 7), (3, 8), (3, 9)]
+    available_versions_text = run(
+        ["pyenv", "install", "--list"], print_stdout=False
+    ).stdout
+    if not available_versions_text:
+        raise ValueError("'pyenv install --list' returned nothing???")
+
+    available_versions = [line.strip() for line in available_versions_text.splitlines()]
+    available_cpython_versions = filter(
+        None, map(cpython_version_re.match, available_versions)
+    )
+
+    available_cpython_version_tuples: List[CPythonVersion] = []
+    for match in available_cpython_versions:
+        cpython_version = CPythonVersion(
+            major=int(match["major"]),
+            minor=int(match["minor"]),
+            micro=int(match["micro"]),
+        )
+        available_cpython_version_tuples.append(cpython_version)
+
+    available_stable_cpython_versions: Dict[Tuple[int, int], List[CPythonVersion]] = {
+        branch_tuple: [] for branch_tuple in stable_branch_tuples
+    }
+    for branch_tuple in available_stable_cpython_versions:
+        versions_in_branch = sorted(
+            version
+            for version in available_cpython_version_tuples
+            if version[:2] == branch_tuple
+        )
+        available_stable_cpython_versions[branch_tuple].extend(versions_in_branch)
+
+    if not all(map(len, available_stable_cpython_versions.values())):
+        raise ValueError(
+            "a stable branch doesn't have any versions available:\n"
+            f"{available_stable_cpython_versions}"
+        )
+
+    def max_version(versions: List[CPythonVersion]) -> Tuple[int, int, int]:
+        "find the most recent version in each supported branch"
+        return max((v.major, v.minor, v.micro) for v in versions)
+
+    supported_cpython_versions = sorted(
+        map(max_version, available_stable_cpython_versions.values()), reverse=True
+    )
+
+    return [
+        ".".join(map(str, tuple(version))) for version in supported_cpython_versions
+    ]
+
+
+def pip_install(args: List[str]) -> None:
+    "run pip install with the specified arguments"
+    run([sys.executable, "-m", "pip", "install", "--user", "--upgrade"] + args)
+
+
+def pipx_install(args: List[str]) -> None:
+    "run pipx install with the specified arguments"
+    run([sys.executable, "-m", "pipx", "install"] + args)
+
+
+def install_poetry() -> None:
+    "install poetry tool from https://python-poetry.org"
+    # Upgrade pip
+    pip_install("pip setuptools wheel".split())
+    pip_install(["pipx"])
+    pipx_install(["poetry"])
+    run([sys.executable, "-m", "pipx", "ensurepath"], check=False)
+    pipx_shims = (Path().home() / ".local" / "bin").resolve(strict=True)
+    # Add pipx shims to PATH
+    add_to_path(pipx_shims)
+    assert check_present("poetry", strict=False), "pipx paths not configured"
+
+
+def main() -> None:
+    "run main checks and tests, but no official builds"
+    # Install pyenv for the appropriate platform
+    if sys.platform in ["linux", "freebsd12"]:
+        install_pyenv_linux()
+    elif sys.platform in ["win32"]:
+        install_pyenv_windows()
+    else:
+        raise NotImplementedError(f"not implemented for platform '{sys.platform}'")
+
+    pyenv_root = Path(run(["pyenv", "root"]).stdout).resolve(strict=True)
+
+    # Install necessary python versions
+    recent_stable_cpython = [
+        version for version in pyenv_stable_cpython() if not version.startswith("3.6")
+    ]
+    run(["pyenv", "install", "3.8.5", "3.7.3"] + recent_stable_cpython)
+    run(["pyenv", "local"] + recent_stable_cpython)
+
+    # Install poetry
+    install_poetry()
+    # Install project dependencies
+    run(["poetry", "install", "--no-interaction", "--verbose"])
+    run(
+        ["poetry", "run", "tox"],
+        env={"PYENV_ROOT": str(pyenv_root), "TOX_PARALLEL_NO_SPINNER": "1"},
+    )
+
+
+if __name__ == "__main__":
+    main()
